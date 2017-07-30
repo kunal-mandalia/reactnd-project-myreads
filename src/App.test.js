95ff101232435ffcb9e3258d390e57c16c13dbd6
kunal-mandalia <kunal.v.mandalia@gmail.com>
2017-07-31 00:07:29 +0100

    test: wrap App in BrowserRouter to stop it crashing on render

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { BrowserRouter } from 'react-router-dom'
 
 /** 
  This course is not designed to teach Test Driven Development. 
@@ -10,7 +11,5 @@ import App from './App'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, div)
 })
-
-
