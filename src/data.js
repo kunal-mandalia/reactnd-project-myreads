c49b4ce7d1ea6f2fa7cffa508077bce3190ce9bf
kunal-mandalia <kunal.v.mandalia@gmail.com>
2017-08-03 01:24:30 +0100

    refactor: data including shelves

---
+++ src/data.js
@@ -0,0 +1,6 @@
+export const shelves = [
+  { shelf: 'currentlyReading', title: 'Currently Reading' },
+  { shelf: 'wantToRead', title: 'Want to Read' },
+  { shelf: 'read', title: 'Read' },
+  { shelf: 'none', title: 'None' }  
+]
