e20523d42a19ca32b239ddf9c4330716a31a2ec2
kunal-mandalia <kunal.v.mandalia@gmail.com>
2017-08-09 02:33:51 +0100

    test: search input updates state on change

--- src/components/SearchBooksBar.test.js
+++ src/components/SearchBooksBar.test.js
@@ -2,12 +2,25 @@ import SearchBooksBar from './SearchBooksBar'
 import renderer from 'react-test-renderer'
 import React from 'react'
 import { MemoryRouter } from 'react-router-dom'
+import { mount } from 'enzyme'
+import MountOptions from '../../helper/mountOptions'
+
+const mockHandleSearch = jest.fn();
+const mockHandleClearSearch = jest.fn();
 
 const props = {
-  searchBooks: () => {}
+  handleSearch: mockHandleSearch,
+  handleClearSearch: mockHandleClearSearch
 }
 
 describe('<SearchBooksBar .../>', () => {
+  it('should update search query on input', () => {
+    const value = 'learning react'
+    const wrapper = mount(<SearchBooksBar {...props} />, MountOptions)
+    wrapper.find('#search-input').simulate('change', {target: {value}})
+    expect(wrapper.state().searchText).toEqual(value)
+  })
+
   it('should match snapshot', () => {
     const tree = renderer.create(<MemoryRouter><SearchBooksBar {...props} /></MemoryRouter>)
     expect(tree).toMatchSnapshot()
